e92f3a09607f0329d416c689a0d22a046ca95dc6
Dékány Balázs <dekanybalazshungary@gmail.com>
2023-10-23 20:23:27 +0200

    input processing: shape

--- E394KZ/Program.cs
+++ E394KZ/Program.cs
@@ -1,5 +1,6 @@
 ﻿using E394KZ;
 using E394KZ.Shapes;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -11,30 +12,120 @@ class Program
 
         var canvas = new Canvas(1920, 1080);
 
-        var circle = new Circle("kor", 12, 12, ConsoleColor.Red, 8);
-        canvas.Draw(circle);
-
-        var dot = new Dot("pont", 10, 12, ConsoleColor.Blue);
-        canvas.Draw(dot);
-
-        var rectangle = new Rectangle("rect", 10, 13, 6, 5, ConsoleColor.Yellow);
-        canvas.Draw(rectangle);
-
-        var line = new Line("lineasd", 0, 0, 7, 14, ConsoleColor.Green);
-        line.Draw(canvas);
-
-        var triangle = new Triangle("triangle", 10, 0, 0, 10, 25, 14, ConsoleColor.White);
-        triangle.Draw(canvas);
-
+        var lastSize = (Console.WindowWidth, Console.WindowHeight);
         DrawFrame();
-        DrawCanvas(canvas, 0, 0, Console.BackgroundColor);
-        DrawPrompt();
-        Console.ReadLine();
+        while (true)
+        {
+
+            DrawCanvas(canvas, 0, 0, Console.BackgroundColor);
+            DrawPrompt();
+            var input = Console.ReadLine() ?? "";
+
+            if (input != "")
+            {
+                if (StartWidth(input.ToLower(), new string[] { "dot", "line", "circle", "rectangle", "triangle" }))
+                {
+                    ShapeParser(input).Draw(canvas);
+                }
+
+                ClearPrompt(input.Length);
+            }
+            else if (input == "" || lastSize != (Console.WindowWidth, Console.WindowHeight))
+            {
+                lastSize = (Console.WindowWidth, Console.WindowHeight);
+                Console.Clear();
+                DrawFrame();
+            }
+        }
     }
+    static bool StartWidth(string text, string[] prefixArray)
+    {
+        foreach(var prefix in prefixArray)
+        {
+            if (text.StartsWith(prefix)) return true;
+        }
+        return false;
+    }
+    static BaseShape? ShapeParser(string text)
+    {
+        var textSplit = text.Split(' ');
+
+        uint x;
+        uint y;
+        ConsoleColor color;
+        string name;
+
+        switch (textSplit[0].ToLower())
+        {
+            case "dot":
+                if (textSplit.Length != 5 && textSplit.Length != 4) throw new InvalidArgumentumCountException("dot",textSplit.Length);
+                x = Convert.ToUInt32(textSplit[1]);
+                y = Convert.ToUInt32(textSplit[2]);
+                color = ConsoleColorParser(textSplit[3]);
+                name = (textSplit.Length == 5) ? textSplit[4] : "";
+                return new Dot(name,x,y,color);
+
+            case "line":
+                if (textSplit.Length != 7 && textSplit.Length != 6) throw new InvalidArgumentumCountException("line", textSplit.Length);
+                x = Convert.ToUInt32(textSplit[1]);
+                y = Convert.ToUInt32(textSplit[2]);
+                var x2 = Convert.ToUInt32(textSplit[3]);
+                var y2 = Convert.ToUInt32(textSplit[4]);
+                color = ConsoleColorParser(textSplit[5]);
+                name = (textSplit.Length == 7) ? textSplit[6] : "";
+                return new Line(name,x,y,color,x2,y2);
+
+            case "rectangle":
+                if (textSplit.Length != 7 && textSplit.Length != 6) throw new InvalidArgumentumCountException("rectangle", textSplit.Length);
+                x = Convert.ToUInt32(textSplit[1]);
+                y = Convert.ToUInt32(textSplit[2]);
+                var width = Convert.ToUInt32(textSplit[3]);
+                var height = Convert.ToUInt32(textSplit[4]);
+                color = ConsoleColorParser(textSplit[5]);
+                name = (textSplit.Length == 7) ? textSplit[6] : "";
+                return new Rectangle(name,x,y,width,height,color);
+
+            case "circle":
+                if (textSplit.Length != 6 && textSplit.Length != 5) throw new InvalidArgumentumCountException("circle", textSplit.Length);
+                x = Convert.ToUInt32(textSplit[1]);
+                y = Convert.ToUInt32(textSplit[2]);
+                var r = Convert.ToUInt32(textSplit[3]);
+                color = ConsoleColorParser(textSplit[4]);
+                name = (textSplit.Length == 6) ? textSplit[5] : "";
+                return new Circle(name, x, y, color, r);
+
+            case "triangle":
+                if (textSplit.Length != 9 && textSplit.Length != 8) throw new InvalidArgumentumCountException("triangle", textSplit.Length);
+                x = Convert.ToUInt32(textSplit[1]);
+                y = Convert.ToUInt32(textSplit[2]);
+                var v2x = Convert.ToUInt32(textSplit[3]);
+                var v2y = Convert.ToUInt32(textSplit[4]);
+                var v3x = Convert.ToUInt32(textSplit[5]);
+                var v3y = Convert.ToUInt32(textSplit[6]);
+                color = ConsoleColorParser(textSplit[7]);
+                name = (textSplit.Length == 9) ? textSplit[8] : "";
+                return new Triangle(name, x, y, color, v2x, v2y, v3x, v3y);
+            default:
+                throw new Exception("this should never be called");
+        }
+    }
+    static ConsoleColor ConsoleColorParser(string colorString)
+    {
+        if (Enum.TryParse<ConsoleColor>(colorString, true, out ConsoleColor color))
+        {
+            return color;
+        }
+        else
+        {
+            throw new InvalidConsoleColorException(colorString);
+        }
+    }
+
 
     static void DrawFrame()
     {
         Console.CursorVisible = false;
+        Console.SetCursorPosition(0, 0);
         var sb = new StringBuilder();
         sb.Append("┏");
         for (int i = 0; i < Console.WindowWidth - 2; i++)
@@ -106,5 +197,11 @@ class Program
             Console.ResetColor();
             Console.SetCursorPosition(1, line++);
         }
+    }
+    static void ClearPrompt(int len)
+    {
+        Console.CursorVisible = false;
+        Console.SetCursorPosition(2, Console.WindowHeight - 2);
+        for(uint i = 0; i < len; i++) { Console.Write(" "); }
     }
 }
---
+++ E394KZ/Shapes/Exceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E394KZ.Shapes
+{
+    class InvalidConsoleColorException : Exception
+    {
+        public InvalidConsoleColorException(string colorString)
+            : base($"Invalid ConsoleColor: {colorString}")
+        {
+        }
+    }
+    class InvalidArgumentumCountException : Exception
+    {
+        public InvalidArgumentumCountException(string shapetype,int argcount)
+            : base($"Invalid argumentum count({argcount}) for {shapetype}") { }
+    }
+}
